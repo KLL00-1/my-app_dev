d38323e63cdd4c268d4339efa1a5d21dee5ad4b5
Lev Kochetkov <levich414@icloud.com>
2026-01-05 00:18:50 +0300

    Changes to be committed: 	modified:   src/app/globals.css 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import BackgroundWaves from '@/components/Background'
-import AnimatedLogo from "@/components/Logo";
 import Footer from "@/components/Footer";
 import Start from "@/components/Start";
 import Head from "next/head";
